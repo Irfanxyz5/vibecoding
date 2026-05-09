439586f895fb38d74e27d9eb7a742de41e893a03
Ipanzx <muhammadirfanirsyad05@gmail.com>
2026-05-09 12:24:27 +0700

    feat: implement user login with session tokens and fix database connection issues

--- src/routes/users-routes.ts
+++ src/routes/users-routes.ts
@@ -1,5 +1,5 @@
 import { Elysia, t } from "elysia";
-import { registerUser } from "../services/users-services";
+import { registerUser, loginUser } from "../services/users-services";
 
 export const userRoutes = new Elysia({ prefix: "/api/users" })
   .post("/", async ({ body, set }) => {
@@ -29,6 +29,35 @@ export const userRoutes = new Elysia({ prefix: "/api/users" })
   }, {
     body: t.Object({
       name: t.String(),
+      email: t.String({ format: 'email' }),
+      password: t.String()
+    })
+  })
+  .post("/login", async ({ body, set }) => {
+    try {
+      const user = await loginUser(body);
+      
+      return {
+        message: "Login user successfully",
+        user,
+      };
+    } catch (error: any) {
+      if (error.message === "email atau password salah") {
+        set.status = 401;
+        return {
+          message: "email atau password salah",
+          error: "Unauthorized",
+        };
+      }
+      
+      set.status = 500;
+      return {
+        message: "Internal Server Error",
+        error: error.message,
+      };
+    }
+  }, {
+    body: t.Object({
       email: t.String({ format: 'email' }),
       password: t.String()
     })
